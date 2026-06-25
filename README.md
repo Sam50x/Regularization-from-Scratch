4fc43f80910b044fff114dc0d860ea47d395591f
Seif Adin Madkour <shadowvoice6@gmail.com>
2026-06-25 17:10:56 +0300

    intro

---
+++ README.md
@@ -0,0 +1,15 @@
+# Regularization: From Scratch
+
+## Intro
+
+In the last video, I built Linear Regression from scratch. It worked. But here's the problem — if you give it enough features, it'll fit your training data perfectly. And that's actually a disaster.
+
+Linear Regression can lie to you. It can tell you it understands your data, but the truth is it just memorizes it. If you give the model new data it won't perform as good as it did before.
+
+What if we forced the model to not feel too confident. Forcing the model to understand rather than just memorize the data you give it.
+
+That's where Regularization comes in. My name is Seif Adin, a Data Science student that's trying to make his way into the tech industry as a machine learning engineer. This is Regularization from Scratch. Let's see how it works and how I'll build it.
+
+## Regularization
+
+## Outro
---
+++ my_linear_regression.py
@@ -0,0 +1,67 @@
+import numpy as np
+import math
+
+class LinearRegression:
+    def __init__(self):
+        self.is_fitted = False
+
+    def fit(self, X, y, epochs=1000, learning_rate=0.01, loss_function='mse'):
+        p = X.shape[1]
+        np.random.seed(42)
+
+        b = np.random.random(1)
+        w = np.random.random(p)
+
+        losses = []
+
+        for epoch in range(epochs):
+            y_pred = self.__predict(X, w, b)
+
+            loss = self.__calculate_loss(y, y_pred, loss_function=loss_function)
+            losses.append(loss)
+
+            grad_b, grad_w = self.__calculate_gradients(X, y, y_pred)
+
+            b = b - learning_rate * grad_b
+            w = w - learning_rate * grad_w
+
+            if epoch % 100 == 0:
+                print(f'{loss_function.upper()} loss at epoch {epoch} = {loss}')
+
+        print(f'{loss_function.upper()} loss at epoch {epochs} = {losses[-1]}')
+
+        self.losses_ = losses
+        self.w_ = w
+        self.b_ = b
+        self.is_fitted = True
+
+    def predict(self, X):
+        if not self.is_fitted:
+            raise ValueError('You need to fit the model first before prediction')
+
+        y_pred = np.dot(X, self.w_.T) + self.b_
+        return y_pred
+
+    def __predict(self, X, w, b):
+        y_pred = np.dot(X, w.T) + b
+        return y_pred
+
+    def __calculate_loss(self, y_true, y_pred, loss_function):
+        n = len(y_true)
+
+        if loss_function == 'mse':
+            loss = np.sum((y_pred - y_true) ** 2) / n
+        elif loss_function == 'rmse':
+            loss = math.sqrt(np.sum((y_pred - y_true) ** 2) / n)
+        elif loss_function == 'mae':
+            loss = np.sum(abs(y_pred - y_true)) / n
+
+        return loss
+
+    def __calculate_gradients(self, X, y_true, y_pred):
+        n = len(y_true)
+
+        grad_b = 2 * (np.sum(y_pred - y_true)) / n
+        grad_w = 2 * (np.dot((y_pred - y_true).T, X)) / n
+
+        return grad_b, grad_w
